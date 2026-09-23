62dc31ac7f2994368f495161eb432088bdbea4f4
Zeenath <fathimazeenathrafeek@gmail.com>
2026-09-23 21:59:44 +0530

    Add Sentry error monitoring integration

---
+++ sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://9c18f5d29d430467a81482328dec7806@o4512136590655488.ingest.us.sentry.io/4512136590983168",
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
